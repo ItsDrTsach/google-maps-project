67bd049ad3ff08847abe530d2930380147fa0c1b
tsach ovadia <67857438+ItsDrTsach@users.noreply.github.com>
2020-10-17 16:40:31 +0300

    .3

---
+++ server/config/config.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "maps",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    define: { underscored: true },
+  },
+  test: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "maps_test",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    define: { underscored: true },
+  },
+  production: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || "maps_production",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    define: { underscored: true },
+  },
+};
