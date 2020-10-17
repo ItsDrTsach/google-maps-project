83ffaea6286d3de8a6763dbb65c2d320952f5a65
tsach ovadia <67857438+ItsDrTsach@users.noreply.github.com>
2020-10-17 17:47:47 +0300

    .

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   test: {
     username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || null,
+    password: process.env.DB_PASSWORD || "1234567890",
     database: process.env.DB_NAME || "maps_test",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
